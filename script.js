581ab47794d1563d47791278e405a7b7ed289746
Sumit Malviya <136370734+Mjlonir29@users.noreply.github.com>
2025-02-06 22:58:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     let message = document.getElementById("message");
 
     
+    
     if (username === "admin" && password === "1234") {
         message.style.color = "lime";
         message.innerText = "Login Successful!";
